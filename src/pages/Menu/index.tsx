706e9765f0c0e11b99c374fa2e26bb14909394ed
matheus <freitas1matheus@gmail.com>
2022-10-24 23:02:58 -0300

    Logo and scss files

---
+++ src/pages/Menu/index.tsx
@@ -0,0 +1,10 @@
+import styles from "./Menu.module.scss"
+import { ReactComponent as Logo } from "assets/logo.svg";
+
+export function Menu() {
+    return (
+        <main>
+            <Logo />
+        </main>
+    )
+}
